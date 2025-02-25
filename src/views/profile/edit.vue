65656c0f838ddd06e6ef95089c0dbee70f667e58
rueen <rueenli@gmail.com>
2025-02-25 19:15:54 +0800

    编辑个人信息

--- src/views/profile/edit.vue
+++ src/views/profile/edit.vue
@@ -2,7 +2,7 @@
  * @Author: diaochan
  * @Date: 2025-02-25 18:25:46
  * @LastEditors: rueen
- * @LastEditTime: 2025-02-25 19:09:39
+ * @LastEditTime: 2025-02-25 19:13:55
  * @Description: 
 -->
 <template>
@@ -10,20 +10,173 @@
     <van-nav-bar
       title="编辑个人信息"
       left-arrow
+      right-text="保存"
       @click-left="onClickLeft"
+      @click-right="onSave"
       :class="$style.navbar"
       fixed
     />
+
+    <div :class="$style.content">
+      <!-- 头像 -->
+      <div :class="$style.avatarItem" @click="onAvatarClick">
+        <span :class="$style.label">头像</span>
+        <div :class="$style.value">
+          <van-image
+            round
+            width="50"
+            height="50"
+            :src="form.avatar"
+          />
+          <van-icon name="arrow" />
+        </div>
+      </div>
+
+      <!-- 基本信息 -->
+      <div :class="$style.formGroup">
+        <div :class="$style.formItem">
+          <span :class="$style.label">名称</span>
+          <van-field
+            v-model="form.name"
+            placeholder="请输入名称"
+            :class="$style.input"
+          />
+        </div>
+
+        <div :class="$style.formItem">
+          <span :class="$style.label">性别</span>
+          <div :class="$style.value" @click="showGenderPicker = true">
+            <span :class="$style.text">{{ form.gender || '请选择' }}</span>
+            <van-icon name="arrow" />
+          </div>
+        </div>
+
+        <div :class="$style.formItem">
+          <span :class="$style.label">职业</span>
+          <van-field
+            v-model="form.occupation"
+            placeholder="请输入职业"
+            :class="$style.input"
+          />
+        </div>
+
+        <div :class="$style.formItem">
+          <span :class="$style.label">城市</span>
+          <div :class="$style.value" @click="showCityPicker = true">
+            <span :class="$style.text">{{ form.city || '请选择' }}</span>
+            <van-icon name="arrow" />
+          </div>
+        </div>
+      </div>
+
+      <!-- 联系方式 -->
+      <div :class="$style.formGroup">
+        <div :class="$style.formItem">
+          <span :class="$style.label">电子邮箱</span>
+          <van-field
+            v-model="form.email"
+            placeholder="请输入邮箱"
+            :class="$style.input"
+          />
+        </div>
+
+        <div :class="$style.formItem">
+          <span :class="$style.label">手机号码</span>
+          <van-field
+            v-model="form.phone"
+            placeholder="请输入手机号"
+            :class="$style.input"
+          />
+        </div>
+
+        <div :class="$style.formItem">
+          <span :class="$style.label">Telegram</span>
+          <van-field
+            v-model="form.telegram"
+            placeholder="请输入Telegram"
+            :class="$style.input"
+          />
+        </div>
+      </div>
+    </div>
+
+    <!-- 性别选择器 -->
+    <van-popup
+      v-model:show="showGenderPicker"
+      position="bottom"
+      round
+    >
+      <van-picker
+        :columns="genderOptions"
+        @confirm="onGenderConfirm"
+        @cancel="showGenderPicker = false"
+        show-toolbar
+      />
+    </van-popup>
+
+    <!-- 城市选择器 -->
+    <van-popup
+      v-model:show="showCityPicker"
+      position="bottom"
+      round
+    >
+      <van-area
+        :area-list="areaList"
+        @confirm="onCityConfirm"
+        @cancel="showCityPicker = false"
+      />
+    </van-popup>
   </div>
 </template>
 
 <script setup>
+import { ref } from 'vue'
 import { useRouter } from 'vue-router'
+import { showToast } from 'vant'
+import { areaList } from '@vant/area-data'
 
 const router = useRouter()
 
+// 表单数据
+const form = ref({
+  avatar: 'https://fastly.jsdelivr.net/npm/@vant/assets/cat.jpeg',
+  name: 'Hi Han',
+  gender: '男',
+  occupation: '自由职业',
+  city: '广东省/深圳市/南山区',
+  email: 'hihan@example.com',
+  phone: '13800138000',
+  telegram: '@hihan'
+})
+
+// 选择器相关
+const showGenderPicker = ref(false)
+const showCityPicker = ref(false)
+
+const genderOptions = ['男', '女', '保密']
+
+// 事件处理
 const onClickLeft = () => {
   router.back()
+}
+
+const onSave = () => {
+  showToast('保存成功')
+  router.back()
+}
+
+const onAvatarClick = () => {
+  showToast('头像上传功能开发中')
+}
+
+const onGenderConfirm = (value) => {
+  form.value.gender = value
+  showGenderPicker.value = false
+}
+
+const onCityConfirm = (values) => {
+  form.value.city = values.map(item => item.name).join('/')
+  showCityPicker.value = false
 }
 </script>
 
@@ -56,6 +209,101 @@ const onClickLeft = () => {
     .van-nav-bar__arrow {
       color: #323233;
       font-size: 18px;
+    }
+
+    .van-nav-bar__text {
+      color: var(--van-primary-color);
+    }
+  }
+}
+
+.content {
+  padding: 12px;
+}
+
+.avatarItem {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 12px 16px;
+  background: #fff;
+  border-radius: 8px;
+  margin-bottom: 12px;
+  cursor: pointer;
+
+  .value {
+    display: flex;
+    align-items: center;
+    gap: 4px;
+
+    .van-icon {
+      color: #969799;
+      font-size: 16px;
+    }
+  }
+}
+
+.formGroup {
+  background: #fff;
+  border-radius: 8px;
+  margin-bottom: 12px;
+  overflow: hidden;
+}
+
+.formItem {
+  display: flex;
+  align-items: center;
+  padding: 12px 16px;
+  border-bottom: 1px solid #f5f6f7;
+
+  &:last-child {
+    border-bottom: none;
+  }
+
+  .label {
+    width: 80px;
+    font-size: 14px;
+    color: #323233;
+  }
+
+  .value {
+    flex: 1;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    cursor: pointer;
+
+    .text {
+      font-size: 14px;
+      color: #323233;
+    }
+
+    .van-icon {
+      color: #969799;
+      font-size: 16px;
+    }
+  }
+}
+
+.input {
+  flex: 1;
+  padding: 0;
+  background: transparent;
+
+  :global {
+    .van-field__body {
+      text-align: right;
+    }
+
+    .van-field__control {
+      text-align: right;
+      font-size: 14px;
+      color: #323233;
+    }
+
+    .van-cell {
+      padding: 0;
+      line-height: normal;
     }
   }
 }
