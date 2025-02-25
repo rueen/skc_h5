c818349c9a0568cd50ac850899716a8028593093
rueen <rueenli@gmail.com>
2025-02-25 18:52:26 +0800

    设置页

---
+++ src/locales/en-US.js
@@ -0,0 +1,18 @@
+export default {
+  tabbar: {
+    home: 'Home',
+    tasks: 'Tasks',
+    groups: 'Groups',
+    profile: 'Profile'
+  },
+  settings: {
+    language: 'Language Settings',
+    selectLanguage: 'Select Language',
+    logout: 'Logout',
+    logoutConfirm: 'Confirm Logout',
+    logoutMessage: 'Are you sure you want to logout?'
+  },
+  common: {
+    cancel: 'Cancel'
+  }
+} 
