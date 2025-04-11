641ec8e3ae83f3250ed9760ad55de92bf795dd7b
rueen <rueenli@gmail.com>
2025-04-11 23:16:00 +0800

    支持跨域

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,10 @@
+/*
+ * @Author: diaochan
+ * @Date: 2025-02-25 10:09:01
+ * @LastEditors: rueen
+ * @LastEditTime: 2025-04-11 23:14:43
+ * @Description: 
+ */
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
@@ -9,5 +16,18 @@ export default defineConfig({
     alias: {
       '@': resolve(__dirname, 'src')
     }
+  },
+  server: {
+    port: 5173,
+    host: '0.0.0.0',
+    proxy: {
+      // 特别注意：如果后端API已经包含/api前缀，则不要重写路径
+      '/api': {
+        target: 'http://api.rueen.cn',
+        changeOrigin: true,
+        // rewrite: (path) => path.replace(/^\/api/, '') // 移除重写规则，保留/api前缀
+      }
+    },
+    cors: true
   }
 })
