fdc933173e0df4a84ff90e4acf8131ab7b32d5b9
rueen <rueenli@gmail.com>
2025-04-18 09:41:36 +0800

    注释

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -2,7 +2,7 @@
  * @Author: diaochan
  * @Date: 2025-03-21 18:36:18
  * @LastEditors: rueen
- * @LastEditTime: 2025-04-14 20:19:10
+ * @LastEditTime: 2025-04-18 08:45:17
  * @Description: 
  */
 import { createI18n } from 'vue-i18n'
@@ -10,7 +10,7 @@ import zh from './zh'
 import en from './en'
 import tl from './tl'
 
-// 获取本地存储的语言设置，默认中文
+// 获取本地存储的语言设置，默认英文
 const defaultLocale = localStorage.getItem('language') || 'en-US'
 
 const i18n = createI18n({
