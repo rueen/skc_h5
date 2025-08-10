46a2c038ff0894dfea5767209d573abcf853af23
rueen <rueenli@gmail.com>
2025-08-10 16:39:03 +0800

    广告位支持外链

--- src/utils/openAd.js
+++ src/utils/openAd.js
@@ -2,7 +2,7 @@
  * @Author: diaochan
  * @Date: 2025-07-20 18:27:25
  * @LastEditors: rueen
- * @LastEditTime: 2025-07-20 18:32:51
+ * @LastEditTime: 2025-08-10 16:38:06
  * @Description: 
  */
 const openAd = (router, item) => {
@@ -16,6 +16,9 @@ const openAd = (router, item) => {
       break;
     case 'taskGroup':
       router.push(`/taskGroups/detail/${content.taskGroupId}`)
+      break;
+    case 'externalLinks':
+      window.location.href = content.externalLinks;
       break;
     default:
   }
